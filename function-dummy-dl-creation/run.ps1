aa37afad8eb787156e9bac270815deec5862414a
Abhijit <udotavi@gmail.com>
2022-08-19 19:04:02 +0530

    First few lines of code

--- function-dummy-dl-creation/run.ps1
+++ function-dummy-dl-creation/run.ps1
@@ -1,3 +1,24 @@
 param([string] $mySbMsg, $TriggerMetadata)
 
-Write-Host "PowerShell ServiceBus queue trigger function processed message: $mySbMsg"
+# will setup the connection to exchange
+function ConnectionSetup() {
+  try {
+      Write-Host "Trying to setup connection.."
+  }
+  catch {
+      Write-Error "$_"
+      return $null
+  }
+
+  return $true
+}
+
+function main(){
+  Write-Host "Calling the main function.."
+  ConnectionSetup
+}
+
+if ($MyInvocation.InvocationName -ne '.')
+{
+main
+}
